6435f2309451403112d17259001263965b7bd4f5
alex <alexey.v.gaydukov@yandex.ru>
2022-07-28 16:35:19 +0300

    added folders and one_day_BIG_ones

--- app/routes.py
+++ app/routes.py
@@ -8,24 +8,36 @@ import os
 @app.route('/')
 #@app.route('/index')
 def index():
-#    date = { 'today': datetime.now().strftime('%d/%m/%Y'),'yesterday'}
     now = datetime.now()
+    cam_name = []
+    for i in os.scandir('./app/static/streams'):
+        if i.is_dir():
+            cam_name.append(i.name)
     date = [(now - timedelta(i)).strftime('%d/%m/%Y')
  for i in range(7)]
     return render_template('index.html',
-	date = date)
+	date = date, cam_name = cam_name)
 
 @app.route('/', methods = ['POST'])
 def parse_request():
     data = request.form
+    cam_name = []
+    for i in os.scandir('./app/static/streams'):
+        if i.is_dir():
+            cam_name.append(i.name)
     #time to count chosen weekday
     chosen = str((datetime.now().weekday() - int(data["date"]))%7 + 1)
+    cam_index = int(data['cam']) - 1
+    dir_name = cam_name[cam_index]
+    # BIG dir name means there is only one weekday
+    if dir_name.isupper:
+        chosen = ''
     file_list ={} 
-    for filename in os.scandir('./app/static/streams'):
+    for filename in os.scandir('./app/static/streams/' + dir_name):
         if filename.is_file() and filename.name.endswith(f'{chosen}.mkv'):
            file_list.update({filename.name:datetime.fromtimestamp(filename.stat()[-1]).strftime('%d%m%Y: %H%M')}) 
     file_list = sorted(file_list.items()) 
-    urls = [url_for('static', filename='streams/'+i[0]) for i in file_list]
+    urls = [url_for('static', filename='streams/' + dir_name + '/' +i[0]) for i in file_list]
     times = [i[1] for i in file_list]
     print(f'urls:{urls}')
     return render_template('playback.html', filename=urls, times=times) #have to make return exit and separate html template
